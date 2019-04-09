cc2242d261bf6794628c972243dccb207135730f
Alejandro Garcia <wateralexpolo@hotmail.com>
2019-04-09 16:07:31 +0200

    GetReminders function

--- MyEasyConnectASP/Pages/Index.cshtml.cs
+++ MyEasyConnectASP/Pages/Index.cshtml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -9,9 +12,42 @@ namespace MyEasyConnectASP.Pages
 {
     public class IndexModel : PageModel
     {
+        public string Reminder { get; set; }
         public void OnGet()
         {
+            string data = "{ WorkerId: '1'}";
+            string contentType = "application/json";
 
+            Reminder = GetReminders("http://localhost:62114/GetReminders",data,contentType);           
+        }
+
+        public string GetReminders(string uri, string data, string contentType, string method = "POST")
+        {
+            byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            request.ContentLength = dataBytes.Length;
+            request.ContentType = contentType;
+            request.Method = method;
+
+            using (Stream requestBody = request.GetRequestStream())
+            {
+                requestBody.Write(dataBytes, 0, dataBytes.Length);
+            }
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+
+        private static IDisposable newHttpClient()
+        {
+            throw new NotImplementedException();
         }
     }
 }
